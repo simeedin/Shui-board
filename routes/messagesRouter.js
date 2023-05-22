d2589ae85fb8d7ba188e9c319f12eee9cb06d660
simeedin <113066728+simeedin@users.noreply.github.com>
2023-05-22 15:08:48 +0200

    date and multipost

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
-const { createMessage, displayChannelMessages } = require("../models/message");
+const moment = require('moment');
+const { addMessage, displayChannelMessages } = require("../models/message");
 const { checkChannelId, checkValidUser } = require("../middleware/channel");
 
 router.post("/", checkChannelId, checkValidUser, (req, res) => {
   const { content, username, channelId } = req.body;
   const messageId = uuidv4();
+  const createdAt = moment().format('YY/MM/DD, h:mm:ss a');
 
   try {
-    createMessage(messageId, content, username, channelId);
-    res.json({ success: true, message: content, username: username });
+    addMessage(messageId, content, username, createdAt, channelId);
+    res.json({ success: true, message: content, username: username, createdAt: createdAt, channelId: channelId });
   } catch (error) {
     res.json({ success: false, message: "Could not create message" });
   }
